0d6e419cf9d06589d49bb1a317a1e02624dce204
wmartin8 <wmartin8@utk.edu>
2014-03-03 22:57:15 -0700

    Updated cron proc
    Added conditional selection for final vs reminder email. Added summary
    email for admin.

--- MotivateYourself/SelfBet/cronproc.php
+++ MotivateYourself/SelfBet/cronproc.php
@@ -10,10 +10,15 @@ $errorString = '';
 // Constructing the email
 $name = 'will';
 $email = 'quarkswithforks@gmail.com';
+// Your name and email address
+$sender = 'The Cheese <selfbet@williammartinengineering.com>'; 
 
 $_SESSION['usersName'] = $name;
 $_SESSION['sqlError'] = false;
 $insertError  = 'will';
+
+$numReminders = 0; // initialize as no emails sent
+$userKeyList = '';
 
 
 // Create connection
@@ -29,9 +34,16 @@ if (!$con)
 else
 {
 	// First, go through database and find users that need reminders sent
-	$query = mysql_query("SELECT emailaddress, username, theBet, amount, endDate, userKey, remindersRemaining, remindersInterval, nextReminder
+	$query = mysql_query("SELECT emailaddress, username, theBet, amount, 
+	endDate, userKey, remindersRemaining, remindersInterval, nextReminder
 	FROM  `slim720_wme`.`selfBet` 
-	WHERE endDate > NOW( ) AND sendReminders IS TRUE AND remindersRemaining > 0 AND nextReminder > NOW()", $con);
+	WHERE 
+		(sendReminders IS TRUE AND
+		remindersRemaining > 0 AND
+		nextReminder < NOW())
+		OR
+		endDate < NOW()"
+		, $con);
 	
 	// Make sure the querry was successful
 	if (!$query) 
@@ -61,57 +73,117 @@ else
 			$remindTmpRemindsRem = $row[6];
 			$remindTmpInterval = $row[7];
 			
-			$emailStr = 'Hey'.$remindTmpUserName.', remember your bet for $'. $remindTmpAmount.' that you\'d '.
-			$remindTmpTheBet.' by '. $remindTmpEndDate.'? You\'ll get '.$remindTmpRemindsRem.' more reminders.';
+			$now = date('Y-m-d H:i:s'); 
+            $nowStr = strtotime($now); 
+            $endStr = strtotime($remindTmpEndDate); 
+            
+            if ($nowStr >= $endStr)
+            {
+            	// Send final email
+                echo "This one is ready!";
+                echo "<br />";
+                $subject = 'Your bet is complete, '.$name.'! How\'d you do?'; 
+                 
+                $userKeyList = $userKeyList .'<br/>Final for key: '. $remindTmpKey .' name: '.$remindTmpUserName.
+                ' Amount: '.$remindTmpAmount.' endDate: '.$remindTmpEndDate;   
+                
+                $html = "
+				<!DOCTYPE html>
+				<html>
+				<body>
+				<a href = 'http://williammartinengineering.com/SelfBet/yes.php'> I did! </a>
+				<button type=\"button\" onclick=\"window.open('Congrats!', 'http://williammartinengineering.com/SelfBet/yes.php');\" >Yes!</button>
+				<br/>
+				<button type=\"button\" onclick=\"window.open('Boo.     ', 'http://williammartinengineering.com/SelfBet/no.php');\" >No:(</button>
+				<a href = 'http://williammartinengineering.com/SelfBet/no.php'> I didn't:( </a>
+				</body>
+				</html>";
 				
-			echo "$emailStr<br />";
-			echo "\n";
-		
-			// Generate and send the email
+				$text = "Select yes or no.";                              
+            } 
+            else 
+            {
+            	// Send a reminder email
+				$emailStr = 'Hey'.$remindTmpUserName.', remember your bet for $'. $remindTmpAmount.' that you\'d '.
+				$remindTmpTheBet.' by '. $remindTmpEndDate.'? You\'ll get '.$remindTmpRemindsRem.' more reminders.<br />';
+					
+				echo "$emailStr<br />";
+				echo "\n";
 			
-			// Compute next reminder date (problem here. Don't use now. Use last reminder date instead)
-			$nextReminderDate = date('Y/m/d', strtotime("+$remindTmpInterval days"));
+				$userKeyList = $userKeyList .'<br/>Reminder for key: '. $remindTmpKey .' name: '.$remindTmpUserName.
+                ' Amount: '.$remindTmpAmount.' endDate: '.$remindTmpEndDate; 
+				
+				// Generate and send the email
+				
+				// Compute next reminder date (problem here. Don't use now. Use last reminder date instead)
+				$nextReminderDate = date('Y/m/d', strtotime("+$remindTmpInterval days"));
+				
+				// Should probably re-calculate number of reminders remaining as well
+				
+				// Update the database with the either the next reminder date
+				$reminderMinus1 = $remindTmpRemindsRem - 1;
+				
+				// COMMENTED OUT FOR EASE OF TESTING
+				//$updSuccess = mysql_query("UPDATE `slim720_wme`.`selfBet` 
+				//SET remindersRemaining=$reminderMinus1, nextReminder=$nextReminderDate
+				//WHERE userKey=$remindTmpKey", $con);
+				
+				// Craft the email
+				$subject = ''.$name.', here\'s a reminder about your SelfBet due in X days!';
+				
+				
+				$html = "Consider yourself reminded";  // HTML version of the email
+
+				$text = "Consider yourself reminded";  // HTML version of the email
+
+			}
 			
-			// Should probably re-calculate number of reminders remaining as well
-			
-			// Update the database with the either the next reminder date
-			$reminderMinus1 = $remindTmpRemindsRem - 1;
-			
-			$updSuccess = mysql_query("UPDATE `slim720_wme`.`selfBet` 
-			SET remindersRemaining=$reminderMinus1, nextReminder=$nextReminderDate
-			WHERE userKey=$remindTmpKey", $con);
+			// All users will receive either a reminder email or a final email
+			$recipient = ''.$name.' <'.$email.'>';
+
+
+			$crlf = "\n";
+			$headers2 = array(
+			'From'          => $sender,
+			'Return-Path'   => $sender,
+			'Subject'       => $subject
+    		);
+
+			// Creating the Mime message
+			$mime = new Mail_mime($crlf);
+
+			// Setting the body of the email
+			$mime->setTXTBody($text);
+			$mime->setHTMLBody($html);
+
+			$body = $mime->get();
+			$headers2 = $mime->headers($headers2);
+
+			// Sending the email
+			//temporarily commented out while working on cron job 
+			$mail =& Mail::factory('mail');
+			$mail->send($recipient, $headers2, $body);                                                           
  		}
 	}
 
-
-
-	//$retval = $row['1']; //Retriev first field
-
-	//$retval = trim($retval); 
-
-	//$ result = mysql_query($con,"SELECT * FROM 'selfBet' WHERE 1");
-	if (1) 
-	{
-		$_SESSION['sqlError'] = $insertError;
-	}
-	else
-	{
-		$_SESSION['sqlError'] = $insertError;
-	}
 
 }
 
 
 
-	// Your name and email address
-	$sender = 'The Cheese <selfbet@williammartinengineering.com>'; 
+if ($numReminders > 0)
+{
+	// Let the admin know that emails have been sent and to who
+	
 	// The Recipients name and email address
-	$recipient = ''.$name.' <'.$email.'>';                          
-	$subject = "CRON";                                   
+	$admin = "Slick Pits";
+	$adminEmail = "wmartin8@utk.edu";
+	$recipient = ''.$admin.' <'.$adminEmail.'>';                          
+	$subject = "CRON PROC Details";                                   
 
-	$html = $row[0];  // HTML version of the email
+	$html = $userKeyList;  // HTML version of the email
 
-	$text = $row[0]; 
+	$text = $userKeyList; 
 
 	$crlf = "\n";
 	$headers2 = array(
@@ -131,8 +203,9 @@ else
 	$headers2 = $mime->headers($headers2);
 
 	// Sending the email
-	/* temporarily commented out while working on cron job 
+	//temporarily commented out while working on cron job 
 	$mail =& Mail::factory('mail');
 	$mail->send($recipient, $headers2, $body);
-	*/
+}
+	
  ?>
