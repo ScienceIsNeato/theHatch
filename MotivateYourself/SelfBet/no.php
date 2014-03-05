07fa7d6f0e61328a7db5ceb694fa83f37765655c
wmartin8 <wmartin8@utk.edu>
2014-03-04 20:29:49 -0700

    Added stub yes, no and payment pages

---
+++ MotivateYourself/SelfBet/no.php
@@ -0,0 +1,8 @@
+<html>
+<header><title>I can't believe how awful you are. Your mother would be so ashamed...</title></header>
+<body>
+<a href="payment.php"><img src="images/failureAndHonest.png" border="0" alt="I am a champion!!!!." width=400>I am honest.</a> 
+or <br/> <br/>
+<a href="payment.php"><img src="images/failureAndALiar.png" border="0" alt="I am a liar." width=400>I am a liar.</a> 
+</body>
+</html>
---
+++ MotivateYourself/SelfBet/yes.php
@@ -0,0 +1,8 @@
+<html>
+<header><title>I can't believe how amazing you are!!!</title></header>
+<body>
+<img src="images/winner.png" border="0" alt="I am a champion!!!!.">
+<br/>
+All should bow down in appreciation of my amazing follow-though-ness!
+</body>
+</html>
